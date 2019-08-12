2587db18f8d4c2a1dce836111370142a2c5be684
cjg9040 <chl8273@kookmin.ac.kr>
2019-08-12 15:02:30 +0900

    a1, cd

--- src/pages/area/a1.vue
+++ src/pages/area/a1.vue
@@ -1,9 +1,127 @@
 <template>
+    <v-container text-xs-center fluid>
+        <v-layout>
+            <v-flex>
+                <v-card>
+                    <v-card-text>
+                        복도
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-layout>
+            <v-flex xs5>
+                <v-card>
+                    <v-card-text>
+                        231호
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex xs5>
+                <v-layout v-for="">
+                    <v-flex>
+
+                    </v-flex>
+                    <v-flex>
+
+                    </v-flex>
+                    <v-flex>
+
+                    </v-flex>
+                </v-layout>
+            </v-flex>
+            <v-flex xs5>
+                <v-card>
+                    <v-card-text>
+                        복도
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex xs5>
+                <v-card>
+                    <v-card-text>
+                        231호
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex xs5>
+                <v-card>
+                    <v-card-text text-center>
+                        232호
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
     export default {
-        name: 'a1'
+        name: 'a1',
+        ALeft: [
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+        ],
+        ARight: [
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+            [
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+                {num: 1, state: 1},
+            ],
+        ],
     }
 </script>
 
--- src/pages/area/cd.vue
+++ src/pages/area/cd.vue
@@ -1,20 +1,32 @@
 <template>
     <v-container text-xs-center fluid>
         <v-layout>
-            <v-flex d-inline-flex xs12>
+            <v-flex d-inline-flex>
                 <v-layout column>
                     <v-layout v-for="Clist in CtotalList" align-start justify-start row fill-height wrap>
                         <v-flex v-for="C in Clist">
-                            <v-card-text class="pa-1 lime lighten-5 text-xs-center">{{C}}</v-card-text>
+                            <v-hover v-slot:default="{ hover }">
+                                <v-card class="elevation-0 lime" :elevation="hover ? 6 : 0">
+                                    <v-card-text class="pa-1 lighten-5 text-xs-center">
+                                        C - <p class="width-50 d-inline-block">{{ C.num }}</p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-hover>
                         </v-flex>
                     </v-layout>
                 </v-layout>
             </v-flex>
-            <v-flex d-inline-flex xs3>
+            <v-flex d-inline-flex>
                 <v-layout column>
                     <v-layout v-for="Dlist in DtotalList">
                         <v-flex v-for="D in Dlist">
-                            <v-card-text class="pa-1 lime lighten-5 text-xs-center">{{D}}</v-card-text>
+                            <v-hover v-slot:default="{ hover }">
+                                <v-card class="elevation-0 lime" :elevation="hover ? 6 : 0">
+                                    <v-card-text class="pa-1 lighten-5 text-xs-center">
+                                        D - <p class="width-50 d-inline-block">{{ D.num }}</p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-hover>
                         </v-flex>
                     </v-layout>
                 </v-layout>
@@ -50,55 +62,148 @@
 <script>
     export default {
         name: 'area_CD',
-        data () {
+        data() {
             return {
                 CtotalList: [
                     [
-                        'C-1', 'C-2', 'C-11', 'C-12', 'C-21',
-                        'C-22', 'C-31', 'C-32', 'C-41', 'C-42',
-                        'C-51', 'C-52', 'C-61', 'C-62', 'C-71',
-                        'C-72', 'C-81', 'C-82', 'C-91', 'C-92'
+                        {num: 1, state: 1},
+                        {num: 2, state: 1},
+                        {num: 11, state: 1},
+                        {num: 12, state: 1},
+                        {num: 21, state: 1},
+                        {num: 22, state: 1},
+                        {num: 31, state: 1},
+                        {num: 32, state: 1},
+                        {num: 41, state: 1},
+                        {num: 42, state: 1},
+                        {num: 51, state: 1},
+                        {num: 52, state: 1},
+                        {num: 61, state: 1},
+                        {num: 71, state: 1},
+                        {num: 72, state: 1},
+                        {num: 81, state: 1},
+                        {num: 91, state: 1},
+                        {num: 92, state: 1},
                     ],
                     [
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1'
+                        {num: 3, state: 1},
+                        {num: 4, state: 1},
+                        {num: 13, state: 1},
+                        {num: 14, state: 1},
+                        {num: 23, state: 1},
+                        {num: 24, state: 1},
+                        {num: 33, state: 1},
+                        {num: 34, state: 1},
+                        {num: 43, state: 1},
+                        {num: 44, state: 1},
+                        {num: 53, state: 1},
+                        {num: 54, state: 1},
+                        {num: 63, state: 1},
+                        {num: 64, state: 1},
+                        {num: 73, state: 1},
+                        {num: 74, state: 1},
+                        {num: 83, state: 1},
+                        {num: 84, state: 1},
+                        {num: 93, state: 1},
+                        {num: 94, state: 1},
                     ],
                     [
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1'
+                        {num: 5, state: 1},
+                        {num: 6, state: 1},
+                        {num: 15, state: 1},
+                        {num: 16, state: 1},
+                        {num: 25, state: 1},
+                        {num: 26, state: 1},
+                        {num: 35, state: 1},
+                        {num: 36, state: 1},
+                        {num: 45, state: 1},
+                        {num: 46, state: 1},
+                        {num: 55, state: 1},
+                        {num: 56, state: 1},
+                        {num: 65, state: 1},
+                        {num: 66, state: 1},
+                        {num: 75, state: 1},
+                        {num: 76, state: 1},
+                        {num: 85, state: 1},
+                        {num: 86, state: 1},
+                        {num: 95, state: 1},
+                        {num: 96, state: 1},
                     ],
                     [
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1'
+                        {num: 7, state: 1},
+                        {num: 8, state: 1},
+                        {num: 17, state: 1},
+                        {num: 18, state: 1},
+                        {num: 27, state: 1},
+                        {num: 28, state: 1},
+                        {num: 37, state: 1},
+                        {num: 38, state: 1},
+                        {num: 47, state: 1},
+                        {num: 48, state: 1},
+                        {num: 57, state: 1},
+                        {num: 58, state: 1},
+                        {num: 67, state: 1},
+                        {num: 68, state: 1},
+                        {num: 77, state: 1},
+                        {num: 78, state: 1},
+                        {num: 87, state: 1},
+                        {num: 88, state: 1},
+                        {num: 97, state: 1},
+                        {num: 98, state: 1},
                     ],
                     [
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1',
-                        'C-1', 'C-1', 'C-1', 'C-1', 'C-1'
+                        {num: 9, state: 1},
+                        {num: 10, state: 1},
+                        {num: 19, state: 1},
+                        {num: 20, state: 1},
+                        {num: 29, state: 1},
+                        {num: 30, state: 1},
+                        {num: 39, state: 1},
+                        {num: 40, state: 1},
+                        {num: 49, state: 1},
+                        {num: 50, state: 1},
+                        {num: 59, state: 1},
+                        {num: 60, state: 1},
+                        {num: 69, state: 1},
+                        {num: 70, state: 1},
+                        {num: 79, state: 1},
+                        {num: 80, state: 1},
+                        {num: 89, state: 1},
+                        {num: 90, state: 1},
+                        {num: 99, state: 1},
+                        {num: 100, state: 1},
                     ]
                 ],
                 DtotalList: [
                     [
-                        'D-1', 'D-2', 'D-11', 'D-12'
+                        {num: 1, state: 1},
+                        {num: 2, state: 1},
+                        {num: 11, state: 1},
+                        {num: 12, state: 1},
                     ],
                     [
-                        'D-1', 'D-2', 'D-11', 'D-12'
+                        {num: 3, state: 1},
+                        {num: 4, state: 1},
+                        {num: 13, state: 1},
+                        {num: 14, state: 1},
                     ],
                     [
-                        'D-1', 'D-2', 'D-11', 'D-12'
+                        {num: 5, state: 1},
+                        {num: 6, state: 1},
+                        {num: 15, state: 1},
+                        {num: 16, state: 1},
                     ],
                     [
-                        'D-1', 'D-2', 'D-11', 'D-12'
+                        {num: 7, state: 1},
+                        {num: 8, state: 1},
+                        {num: 17, state: 1},
+                        {num: 18, state: 1},
                     ],
                     [
-                        'D-1', 'D-2', 'D-11', 'D-12'
+                        {num: 9, state: 1},
+                        {num: 10, state: 1},
+                        {num: 19, state: 1},
+                        {num: 20, state: 1},
                     ]
                 ]
             }
@@ -107,4 +212,11 @@
 </script>
 
 <style scoped>
+    .width-50 {
+        width: 25px;
+    }
+
+    p {
+        margin: 0;
+    }
 </style>
