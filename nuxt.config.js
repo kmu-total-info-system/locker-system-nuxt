8897419558dd89b832d03b05e2b0b9bd40b0663d
cjg9040 <chl8273@kookmin.ac.kr>
2019-08-13 14:48:32 +0900

    locker-system-nuxt-7 [Feature] 모바일 용 레이아웃 - select

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,60 +1,64 @@
 const nodeExternals = require('webpack-node-externals')
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+
 module.exports = {
-  srcDir:'src/',
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'locker-system-nuxt',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
-    ]
-  },
-  plugins: ['~/plugins/vuetify.js'],
-  css: ['~/assets/style/app.styl'],
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    transpile: [/^vuetify/],
-    plugins: [
-      new VuetifyLoaderPlugin()
-    ],
-    extractCSS: true,
-    extend (config, ctx) {
-      // Run ESLint on save , clinet strictly
-      // if (ctx.isDev && ctx.isClient) {
-      //   config.module.rules.push({
-      //     enforce: 'pre',
-      //     test: /\.(js|vue)$/,
-      //     loader: 'eslint-loader',
-      //     exclude: /(node_modules)/
-      //   })
-      // }
-      if (process.server) {
-        config.externals = [
-          nodeExternals({
-            whitelist: [/^vuetify/]
-          })
+    srcDir: 'src/',
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: 'locker-system-nuxt',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project'}
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+            {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
         ]
-      }
     },
-    router: {
-      base: '/',
-      scrollBehavior: function (to, from, savedPosition) {
-        return {x: 0, y: 0}
-      }
+    plugins: ['~/plugins/vuetify.js'],
+    css: ['~/assets/style/app.styl'],
+    /*
+    ** Customize the progress bar color
+    */
+    loading: {color: '#3B8070'},
+    /*
+    ** Build configuration
+    */
+    build: {
+        build: {
+            vendor: ['vuetify'],
+        },
+        transpile: [/^vuetify/],
+        plugins: [
+            new VuetifyLoaderPlugin()
+        ],
+        extractCSS: true,
+        extend(config, ctx) {
+            // Run ESLint on save , clinet strictly
+            // if (ctx.isDev && ctx.isClient) {
+            //   config.module.rules.push({
+            //     enforce: 'pre',
+            //     test: /\.(js|vue)$/,
+            //     loader: 'eslint-loader',
+            //     exclude: /(node_modules)/
+            //   })
+            // }
+            if (process.server) {
+                config.externals = [
+                    nodeExternals({
+                        whitelist: [/^vuetify/]
+                    })
+                ]
+            }
+        },
+        router: {
+            base: '/',
+            scrollBehavior: function (to, from, savedPosition) {
+                return {x: 0, y: 0}
+            }
+        }
     }
-  }
 }
---
+++ src/components/Select.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-select label="사물함 선택하기"
+              filled
+              :items="items"
+              item-text="num"
+              item-value="num"
+              persistent-hint
+              autocomplete>
+        <template slot="selection" slot-scope="data">
+            {{data.item.group}} - {{ data.item.num }}
+        </template>
+        <template slot="item" slot-scope="data">
+            <template>
+                <v-list-tile-content>
+                    <v-list-tile-title>{{data.item.group}} - {{ data.item.num }}
+                    </v-list-tile-title>
+                </v-list-tile-content>
+            </template>
+        </template>
+    </v-select>
+</template>
+
+<script>
+    export default {
+        name: "Select",
+        props: [
+            'items',
+        ],
+        data() {
+            return {
+                e11: {group: ''},
+                people: [
+                    {header: 'Group 1'},
+                    {name: 'Sandra Adams', group: 'Group 1'},
+                    {name: 'Ali Connors', group: 'Group 1'},
+                    {name: 'Trevor Hansen', group: 'Group 1'},
+                    {name: 'Tucker Smith', group: 'Group 1'},
+                    {divider: true},
+                    {header: 'Group 2'},
+                    {name: 'Britta Holt', group: 'Group 2'},
+                    {name: 'Jane Smith ', group: 'Group 2'},
+                    {name: 'John Smith', group: 'Group 2'},
+                    {name: 'Sandra Williams', group: 'Group 2'}
+                ]
+            }
+        },
+        watch: {
+            e11: function (newValue, oldValue) {
+                console.log(newValue, oldValue)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
